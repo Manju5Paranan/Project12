34a94a2a3190c2c4afd28110f97c6b4d4adab79a
ManjuKarthik Github <manjukarthikraja2508@gmail.com>
2026-04-08 10:19:40 +0530

    ObjtoC

---
+++ ObjtoC.java
@@ -0,0 +1,35 @@
+class Box
+{
+    double width,height,depth;
+
+    Box(double width,double height,double depth)
+    {
+        this.width=width;
+        this.height=height;
+        this.depth=depth;
+    }
+    double volume()
+    {
+        return width*depth*height;
+    }
+    
+    Box(Box ob)
+    {
+        width=ob.width;
+        height=ob.height;
+        depth=ob.depth;
+    }
+
+}
+class ObjtoC{
+    public static void main(String[] args) {
+        {
+            Box b1=new Box(12,3,4);
+            Box b2=new Box(b1);
+            double v=b1.volume();
+            System.out.println(v);
+            v=b2.volume();
+            System.out.println(v);
+        }
+    }
+}
